970c6ae44d361c7b143387f3b7a91e9e3ac549a1
Jason Yoo <architectophile@gmail.com>
2023-08-28 08:44:36 +0900

    add: mkdir command

--- set-c4-dataset.sh
+++ set-c4-dataset.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Check if the directory "./datasets" exists
+if [ ! -d "./datasets" ]; then
+  # Create the directory if it does not exist
+  mkdir "./datasets"
+fi
+
 # Navigate to the target directory
-cd "./datasets/c4"
+cd "./datasets"
 
 # Run git clone
 git clone https://huggingface.co/datasets/allenai/c4
